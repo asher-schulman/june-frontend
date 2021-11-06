244a1cb153d754dab1fff2a07fb2957d28a6b067
asher8346 <asher.schulman@gmail.com>
2021-11-06 10:18:14 -0500

    fixed CORS origin renaming in heroku app and backend api hyperlink

--- src/Components/AboutPage.jsx
+++ src/Components/AboutPage.jsx
@@ -6,7 +6,7 @@ function AboutPage() {
     return (
         <Container className="my-3">
             <h1>Welcome to my weather app!</h1>
-            <p>This app was made as a project for General Assembly's SEI bootcamp. It utilizes current weather data from openweathermap's free API. Built with React and Bootstrap on the frontend and a nodeJS/Express backend. Both backend and frontend hosted on heroku. With a MongoDB Atlas Cluster running the databse for eventual CRUD operations. Checkout the <a href="https://june-backend.herokuapp.com//api">backend api</a> I created for this app. </p>
+            <p>This app was made as a project for General Assembly's SEI bootcamp. It utilizes current weather data from openweathermap's free API. Built with React and Bootstrap on the frontend and a nodeJS/Express backend. Both backend and frontend hosted on heroku. With a MongoDB Atlas Cluster running the databse for eventual CRUD operations. Checkout the <a href="https://june-backend.herokuapp.com/api">backend api</a> I created for this app. </p>
             {/* <TestComponent/> */}
         </Container>
     )
